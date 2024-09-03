38f4a4f8d190495c135b36d5d205e03d1e623ca3
lanx387374 <149005325+lanx387374@users.noreply.github.com>
2024-09-03 21:46:48 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,14 +1,23 @@
+local maxDistance = 50.0 
+
 RegisterNetEvent('headshot:applyDamage')
 AddEventHandler('headshot:applyDamage', function(playerId)
+    local src = source
     local ped = GetPlayerPed(GetPlayerFromServerId(playerId))
+    local shooterPed = GetPlayerPed(src)
 
-    if ped and DoesEntityExist(ped) then
+    if ped and shooterPed and DoesEntityExist(ped) and DoesEntityExist(shooterPed) then
         local health = GetEntityHealth(ped)
-        if health > 0 then
-            local headshotDamage = 100 
 
-            local newHealth = math.max(health - headshotDamage, 0)
-            SetEntityHealth(ped, newHealth)
+        if health > 0 then
+            local shooterCoords = GetEntityCoords(shooterPed)
+            local targetCoords = GetEntityCoords(ped)
+            local distance = #(shooterCoords - targetCoords) 
+
+            if distance <= maxDistance then
+                local headshotDamage = 100
+                local newHealth = math.max(health - headshotDamage, 0)
+                SetEntityHealth(ped, newHealth)
 
             if newHealth <= 0 then
                 SetEntityHealth(ped, 0)
