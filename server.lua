2b31d9283c5fd3109fa59e7b5e6a5bf1fdeddc02
lanx387374 <149005325+lanx387374@users.noreply.github.com>
2024-09-08 18:19:22 +0300

    fixed server.lua

--- server.lua
+++ server.lua
@@ -3,8 +3,8 @@ local maxDistance = 50.0
 RegisterNetEvent('headshot:applyDamage')
 AddEventHandler('headshot:applyDamage', function(playerId)
     local src = source
-    local ped = GetPlayerPed(GetPlayerFromServerId(playerId))
-    local shooterPed = GetPlayerPed(src)
+    local ped = GetPlayerPed(GetPlayerFromServerId(playerId)) 
+    local shooterPed = GetPlayerPed(GetPlayerFromServerId(src)) 
 
     if ped and shooterPed and DoesEntityExist(ped) and DoesEntityExist(shooterPed) then
         local health = GetEntityHealth(ped)
@@ -17,26 +17,26 @@ AddEventHandler('headshot:applyDamage', function(playerId)
             if distance <= maxDistance then
                 local headshotDamage = 100
                 local newHealth = math.max(health - headshotDamage, 0)
+
+                if newHealth <= 0 then
+                    TriggerClientEvent('esx:showNotification', playerId, 'تم قتلك بسبب إصابة في الرأس!')
+                end
+
                 SetEntityHealth(ped, newHealth)
 
                 if newHealth <= 0 then
                     SetEntityHealth(ped, 0)
-                    
--- notify
-                    TriggerClientEvent('esx:showNotification', playerId, 'تم قتل اللاعب بسبب تلقيه إصابة في الرأس!')
-
                     local playerName = GetPlayerName(GetPlayerFromServerId(playerId))
                     local webhookUrl = 'ur_webhook_goes_here'
                     local message = {
                         ["username"] = "Headshot Logger",
                         ["content"] = "**" .. playerName .. "** تم قتله بسبب إصابة في الرأس!"
                     }
-                    
+
                     PerformHttpRequest(webhookUrl, function(err, text, headers) 
                     end, 'POST', json.encode(message), { ['Content-Type'] = 'application/json' })
                 end
             else
---notify if the distance is far
                 TriggerClientEvent('esx:showNotification', src, 'المسافة بعيدة جدًا لتنفيذ هجوم.')
             end
         end
